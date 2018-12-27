8b7fbe62c66d2a9bf78699abccf0f31d130d760f
Hyeyoon Jung <hyeyoonjungkr@gmail.com>
2018-12-27 16:34:56 +0900

    Add quiz

--- js/rooms.js
+++ js/rooms.js
@@ -2,7 +2,6 @@ const app = new Vue({
   el: '#app',
   data: {
     showChat: false,
-    hasKey: false,
   },
   methods: {
     activeChat(text) {
@@ -16,28 +15,12 @@ const app = new Vue({
         onComplete: (self) => {vm.closeChat(self)},
       });
     },
-    getKey() {
-      this.activeChat("밤코카드를 획득했다.");
-      setTimeout(() => {
-        this.hasKey = true;
-      }, 1000)
-    },
-    openDoor() {
-      if (this.hasKey) {
-        this.activeChat("삑. 문이 열렸다.");
-        setTimeout(() => {
-          window.location.href = "/front.html";
-        }, 3000)
-      } else {
-        this.activeChat("경비를 해제해야할 것 같다.");
-      }
-    },
     closeChat(self) {
       setTimeout(() => {
         this.showChat = false;
         const chatText = document.querySelector('.chat-text');
         chatText.innerText = '';
-      }, 700)
+      }, 1000)
     }
   },
   mounted() {
