7d87db92d77e912e710574486c191881f9500576
Hyeyoon Jung <hyeyoonjungkr@gmail.com>
2018-12-27 15:55:24 +0900

    Add door.js

---
+++ js/door.js
@@ -0,0 +1,43 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    showChat: false,
+    hasKey: false,
+  },
+  methods: {
+    activeChat(text) {
+      const vm = this;
+      vm.showChat = true;
+      const typed = new Typed('.chat-text', {
+        strings: ["", text],
+        typeSpeed: 40,
+        backDelay: 500,
+        fadeOut: true,
+        onComplete: (self) => {vm.closeChat(self)},
+      });
+    },
+    getKey() {
+      this.hasKey = true;
+      this.activeChat("밤코카드를 획득했다.");
+    },
+    openDoor() {
+      if (this.hasKey) {
+        this.activeChat("삑. 문이 열렸다.");
+        setTimeout(() => {
+          window.location.href = "/crong.html";
+        }, 3000)
+      } else {
+        this.activeChat("경비를 해제해야할 것 같다.");
+      }
+    },
+    closeChat(self) {
+      setTimeout(() => {
+        this.showChat = false;
+        const chatText = document.querySelector('.chat-text');
+        chatText.innerText = '';
+      }, 700)
+    }
+  },
+  mounted() {
+  }
+})
