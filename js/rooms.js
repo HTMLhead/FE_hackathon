ece60caf46d3dd6c622c3a2e97a5920e53c3ecdc
Head <mrdoo309@gmail.com>
2018-12-27 17:19:08 +0900

    make js

--- js/rooms.js
+++ js/rooms.js
@@ -21,6 +21,20 @@ const app = new Vue({
         const chatText = document.querySelector('.chat-text');
         chatText.innerText = '';
       }, 1000)
+    },
+    open() {
+      var firQues = prompt('세상에서 가장 지루한 중학교는?')
+      var secQues = prompt('창문을 깼는데 피가 안 나오면?')
+      var trdQues = prompt('반성문을 영어로 하면?')
+      if(firQues === '로딩중') {
+        if(secQues === '윈도우xp') {
+          if(trdQues === '글로벌') {
+            window.location.herf = './ending.html'
+          }
+        }
+      } else{
+        alert('열리지않는다.')
+      }
     }
   },
   mounted() {
