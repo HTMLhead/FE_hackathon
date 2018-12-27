5c53d3b96c4e55c3ed9fe67c59d11ed453d0b756
Head <mrdoo309@gmail.com>
2018-12-27 17:33:40 +0900

    change problem

--- js/rooms.js
+++ js/rooms.js
@@ -25,10 +25,10 @@ const app = new Vue({
     open() {
       var firQues = prompt('세상에서 가장 지루한 중학교는?')
       var secQues = prompt('창문을 깼는데 피가 안 나오면?')
-      var trdQues = prompt('반성문을 영어로 하면?')
+      var trdQues = prompt('세상에서 가장 가난한 임금은?')
       if(firQues === '로딩중') {
         if(secQues === '윈도우xp') {
-          if(trdQues === '글로벌') {
+          if(trdQues === '최저임금') {
             alert('자물쇠가 열렸다. 안에는 시험지가 놓여있었다.')
             window.location.href = './ending.html'
           }
