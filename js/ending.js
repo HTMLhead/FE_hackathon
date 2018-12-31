1974987e2925e0f44f2423f50fd15d969f3adc37
sejungkim <smilesejung@gmail.com>
2018-12-31 18:56:40 +0900

    엔딩 마지막 대사 부분 버그 수정
    - 인트로와 마찬가지로 엔딩 마지막 장면에서 클릭하면 대사가 다 끝나기도 전에
      화면이 넘어가버리는 버그 수정

--- js/ending.js
+++ js/ending.js
@@ -24,7 +24,7 @@ var app = new Vue({
       },
       {
         imageUrl: 'ending',
-        text: ["", "지난 시험에 10점을 맞은 H 군은 이번 시험은 잘 보기 위해 시험지를 훔치러 늦은 밤 코드스쿼드에 잠입하기로 결심했다."]
+        text: ["", "지난 시험에 10점을 맞은 H 군은 이번 시험은 잘 보기 위해 시험지를 훔치러 늦은 밤 코드스쿼드에 잠입하기로 결심했다..."]
       },
     ],
     activeIndex: 0,
@@ -35,7 +35,7 @@ var app = new Vue({
       if (this.isLoaded && this.activeIndex < 5) {
         this.activeIndex++
         this.activeChat();
-      } else if (this.activeIndex === 5) {
+      } else if (this.isLoaded && this.activeIndex === 5) {
         window.location.href = "./end.html";
       }
       if (this.activeIndex === 3) {
