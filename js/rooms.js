57625940bb5fe0e4f4fa738e396d21e22c475678
Hyeyoon Jung <hyeyoonjungkr@gmail.com>
2019-01-01 15:52:08 +0900

    모달 컴포넌트 추가

--- js/rooms.js
+++ js/rooms.js
@@ -2,6 +2,25 @@ const app = new Vue({
   el: '#app',
   data: {
     showChat: false,
+    showModal: false,
+    questions: [
+      {
+        question: '1. 세상에서 가장 지루한 중학교는?',
+        input: '',
+        answer: '로딩중',
+      },
+      {
+        question: '2. 창문을 깼는데 피가 안 나오면?',
+        input: '',
+        answer: '윈도우xp',
+      },
+      {
+        question: '3. 세상에서 가장 가난한 임금은?',
+        input: '',
+        answer: '최저임금',
+      },
+    ],
+    wrongAnswer: false,
   },
   methods: {
     activeChat(text) {
@@ -43,6 +62,38 @@ const app = new Vue({
         setTimeout(() => {
           alert('열리지 않는다.');
         }, 200)
+      }
+    },
+    resetAnswer() {
+      this.questions.forEach((item) => {
+        item.input = '';
+      })
+    },
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+      this.resetAnswer();
+    },
+    submitAnswer() {
+      const correctAnswers = this.questions.filter(item => {
+        return item.input === item.answer;
+      });
+      if (correctAnswers.length === this.questions.length) {
+        const LockOpen = new Audio('./sound/lockOpen.mp3')
+        LockOpen.play();
+        setTimeout(() => {
+          window.location.href = './ending.html'
+        }, 500)
+      } else {
+        this.wrongAnswer = true;
+        const Lock = new Audio('./sound/lock.mp3');
+        Lock.play();
+        this.resetAnswer();
+        setTimeout(() => {
+          this.wrongAnswer = false;
+        }, 500);
       }
     }
   },
