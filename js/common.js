2e88d20f723267843194749a8a65f42279b2abda
sejungkim <smilesejung@gmail.com>
2019-01-04 15:17:11 +0900

    시작 페이지에서 게임 방법 버튼 클릭 토글로 동작하도록 변경

--- js/common.js
+++ js/common.js
@@ -1,8 +1,14 @@
 const app = new Vue({
   el: '#app',
+  data: {
+    show: ''
+  },
   methods: {
     restart() {
       window.location.href = "./index.html";
+    },
+    showGameRule() {
+      this.show = this.show ? '' : ' show';
     }
   }
 })
