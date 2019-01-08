ca2382e8e78fdee229e9e0e5ff9f9dd4a07d362b
sejungkim <smilesejung@gmail.com>
2019-01-08 14:57:45 +0900

    intro 페이지 typed 관련 부분 코드 오류 수정

--- js/intro.js
+++ js/intro.js
@@ -4,27 +4,27 @@ const app = new Vue({
     stories: [
       {
         imageUrl: 'intro1',
-        text: ["", "매달 마지막 주 목요일에 프론트엔드 클래스는 쪽지시험을 본다.", "지난 시험에 10점을 맞은 H 군은 이번 시험은 잘 보기 위해 시험지를 훔치러 늦은 밤 코드스쿼드에 잠입하기로 결심했다."],
+        text: ["매달 마지막 주 목요일에 프론트엔드 클래스는 쪽지시험을 본다.", "지난 시험에 10점을 맞은 H 군은 이번 시험은 잘 보기 위해 시험지를 훔치러 늦은 밤 코드스쿼드에 잠입하기로 결심했다."]
       },
       {
         imageUrl: 'intro2',
-        text: ["", "문은 잠겨있다.", "옆 24시 감자탕집을 통해 들어간다."],
+        text: ["문은 잠겨있다.", "옆 24시 감자탕집을 통해 들어간다."]
       },
       {
         imageUrl: 'intro3',
-        text: ["", "엘리베이터를 타고 올라간다."],
+        text: ["엘리베이터를 타고 올라간다."]
       },
       {
         imageUrl: 'intro4',
-        text: ["", "4층을 누른다."],
+        text: ["4층을 누른다."]
       },
       {
         imageUrl: 'intro5',
-        text: ["", "코드스쿼드에 도착했다.", "시험지를 훔치러 들어가 볼까?"],
-      },
+        text: ["코드스쿼드에 도착했다.", "시험지를 훔치러 들어가 볼까?"]
+      }
     ],
     activeIndex: 0,
-    isLoaded: false,
+    isLoaded: false
   },
   methods: {
     addIndex() {
@@ -41,6 +41,7 @@ const app = new Vue({
       const typed = new Typed('.chat-text', {
         strings: vm.stories[vm.activeIndex].text,
         typeSpeed: 40,
+        startDelay: 500,
         backDelay: 500,
         fadeOut: true,
         onComplete: () => { vm.doneLoading() },
