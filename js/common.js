6d621b541c3de85d75b9a438e6295c52e0d22775
Head <mrdoo309@gmail.com>
2019-01-10 19:42:43 +0900

    css와 js를 이용한 화면전환
    display클래스를 넣어주고 빼주고를 사용하여 화면전환이 원활하게 일어나도록 수정

--- js/common.js
+++ js/common.js
@@ -1,8 +1,14 @@
+//querySelector너무 길어요
+function qs(value) {
+  return document.querySelector(value)
+}
+const spinner = qs('.spinner')
+const conatiner = qs('.container')
 let fontLoadChecker = setInterval(() => {
-  console.log(document.fonts.status)
   if(document.fonts.status === 'loaded') {
     clearInterval(fontLoadChecker);
-    console.log('hihihihi')
+    spinner.classList.add('display')
+    conatiner.classList.remove('display')
   }
 },100)
 const app = new Vue({
