c38c399f115e20a68f7801ebadee988164e9a045
HTMLhead <mrdoo309@gmail.com>
2022-12-08 11:32:18 +0900

    update: 뷰 패키지 버전 수정

--- js/catcha.js
+++ js/catcha.js
@@ -4,36 +4,38 @@ var app = new Vue({
     stories: [
       {
         imageUrl: "intro1",
-        text: ["헐레벌떡 학원을 빠져나왔다."]
+        text: ["헐레벌떡 학원을 빠져나왔다."],
       },
       {
         imageUrl: "intro1",
-        text: ["그래, 지금이라도 집으로 돌아가자."]
+        text: ["그래, 지금이라도 집으로 돌아가자."],
       },
       {
         imageUrl: "catcha",
-        text: ["'잡았다 요놈!'"]
+        text: ["'잡았다 요놈!'"],
       },
       {
         imageUrl: "catcha",
-        text: ["..."]
+        text: ["..."],
       },
       {
         imageUrl: "blank",
-        text: ["화들짝 놀라며 H 군은 잠에서 깨어났다."]
+        text: ["화들짝 놀라며 H 군은 잠에서 깨어났다."],
       },
       {
         imageUrl: "blank",
-        text: ["매달 마지막 주 목요일에 프론트엔드 클래스는 쪽지시험을 본다."]
+        text: ["매달 마지막 주 목요일에 프론트엔드 클래스는 쪽지시험을 본다."],
       },
       {
         imageUrl: "blank",
-        text: ["지난 시험에 10점을 맞은 H 군은 이번 시험은 잘 보기 위해 시험지를 훔치러 늦은 밤 코드스쿼드에 잠입하기로 결심했다..."]
-      }
+        text: [
+          "지난 시험에 10점을 맞은 H 군은 이번 시험은 잘 보기 위해 시험지를 훔치러 늦은 밤 코드스쿼드에 잠입하기로 결심했다...",
+        ],
+      },
     ],
     activeIndex: 0,
     isLoaded: false,
-    catchedSound: new Audio('./sound/catcha.wav')
+    catchedSound: new Audio("./sound/catcha.wav"),
   },
   methods: {
     addIndex() {
@@ -58,14 +60,14 @@ var app = new Vue({
         fadeOut: true,
         onComplete: () => {
           vm.doneLoading();
-        }
+        },
       });
     },
     doneLoading() {
       this.isLoaded = true;
-    }
+    },
   },
   mounted() {
     this.activeChat();
-  }
+  },
 });
